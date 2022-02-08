846a631e8026e524d1733234291bb6f4b05aa431
shochendoner <shochendoner@gmail.com>
2022-02-08 07:34:43 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!--Splitting the header and footer into separate documents makes things easier!-->
+<?php
+  include_once 'header.php';
+?>
+
+<section class="index-intro">
+  <h1>Surviving March</h1>
+  <p>Brought to you by the "Pockets Aint Empty Podcast"!</p>
+</section>
+
+<section class="index-categories">
+  <h2>Make Your Selections</h2>
+  <div class="card-body p-0 mt-3">
+  <p class="card-text lead text-muted text-center hm mt-2">Game Rules: </p>
+  <p class="card-text">
+    "Entry Fee is $20. Preferable method of payment is venmo (@Dhoch7). If you do not have venmo, please coordinate with someone who does because that is the easiest way for me to pay out the winner(s).
+
+    1. Like any other survivor pool, once you pick a team once, you may not pick that same team again.
+    2. The picks must be made prior to tip off of the first game for every single day. Even if your team/teams do not play until 10:00 pm, you must put in your pick/picks before the noon games because knowing who their opponents would be in the next round could give an unfair advantage.All picks can be emailed to me or sent via text.If you would prefer to text me your picks, please email me your number if I do not already have it.  My number has not changed if you have used this method previously.
+    3. Please remember, that the picks are made on a day-by-day basis and not round-by-round.
+    4.a. The below is the set-up for how many picks are required to be made each day if you are correct:
+
+    Friday - 2 picks
+    Saturday - 2 picks
+    Sunday - 1 pick
+    Monday - 1 pick
+    Every following day is also 1 pick.
+
+    b.If you pick incorrectly, you are out unless you buy back in (see rule 5).
+
+    5.Buying back in:
+
+    Each player has the chance to buy back-in, but it will cost you another entry fee ($20) for each buy-in as well as more games to pick. Below is the schedule for how many games you will need to pick in order to buy back in and advance:
+
+    a) If you get any wrong on Friday, must pick 4 winners on Saturday. If all 4 on Saturday are correct, you will advance and will only need to pick 1 winner Sunday.
+
+    b) If you get any wrong on Saturday, must pick 5 winners on Sunday. If all 5 on Sunday are correct, you will advance and will only need to pick 1 winner Monday.
+
+    c) If you get any wrong Sunday, must pick 6 winners on Monday. If all 6 on Monday are correct, you will advance and will only need to pick one winner on Saturday of the 2nd week.
+
+    This is a survivor pool, and the reason for the above is to make it more difficult for those who have already picked a loser. You can buy-in as many times as you would like, but the last day to buy in would be Monday of the first weekend.  If you pick any loser on Monday or beyond, you can not buy back in.  And remember, just because you buy back in, it does not reset the teams you can choose from.  Each team you have chosen before is not available for selection again.  
+
+    In the event that you have advanced and do not have anyone else remaining to choose from because you have chosen all of the teams before that round, you would still tie any player that does not correctly guess the next round.  For instance, if I were to have no remaining teams left after Sunday, but one other person was left and they did not pick a winner on Saturday, we would tie and split the pot."
+    </div>
+    <div class="index-categories-list">
+    <div>
+      <h3>Your Picks</h3>
+    </div>
+  </div>
+</section>
+
+<?php
+  include_once 'footer.php';
+?>
