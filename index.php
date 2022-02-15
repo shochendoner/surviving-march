26a28eddadcd6c910af6f45a1cfd49c0151b6ffc
shochendoner <shochendoner@gmail.com>
2022-02-15 12:20:20 -0500

    commit

--- index.php
+++ index.php
@@ -13,41 +13,39 @@
 </section>
 <section class="rules-text-01">
     
-  
-       <p> 1. Like any other survivor pool, once you pick a team once, you may not pick that same team again.
-        2. The picks must be made prior to tip off of the first game for every single day. Even if your team/teams do not play until 10:00 pm, you must put in your pick/picks before the noon games because knowing who their opponents would be in the next round could give an unfair advantage.All picks can be emailed to me or sent via text.If you would prefer to text me your picks, please email me your number if I do not already have it.  My number has not changed if you have used this method previously.
-        3. Please remember, that the picks are made on a day-by-day basis and not round-by-round.
-        4.a. The below is the set-up for how many picks are required to be made each day if you are correct:</p>
-    
-    <div>
+      <ol>
+       <li>Like any other survivor pool, once you pick a team once, you may not pick that same team again.</li>
+       <li>Picks are made on a day-by-day basis and not round-by-round.</li>
+       <li>Picks must be made prior to tip off of the first game for every single day. Even if your team/teams do not play until night.</li>
+       <li>Below is how many picks are required to be made each day if you are correct:</li>
+        <ul>
+          <li>Friday - 2 picks</li>
+          <li>Saturday - 2 picks</li>
+          <li>Sunday - 1 pick</li>
+          <li>Monday - 1 pick</li>
+          <li>Every following day is also 1 pick.</li>
+        </ul>
+        <li>If you pick incorrectly, you are out unless you re-enter the game.</li>
+      </ol>
+    <h4>Re-entering the Game:</h4>
+      <ol>  
+        <li>Each player has the chance to rejoin the game, but it will cost you another entry for each re-entry as well as more games to pick.</li>
+        <li>Below is the schedule for how many games you will need to pick in order re-enter the game and advance:</li>
 
       <ul>
-        <li>Friday - 2 picks</li>
-        <li>Saturday - 2 picks</li>
-        <li>Sunday - 1 pick</li>
-        <li>Monday - 1 pick</li>
+          <li>If you get any wrong on Friday, must pick 4 winners on Saturday. If all 4 on Saturday are correct, you will advance and will only need to pick 1 winner Sunday.</li>
+
+          <li>If you get any wrong on Saturday, must pick 5 winners on Sunday. If all 5 on Sunday are correct, you will advance and will only need to pick 1 winner Monday.</li>
+
+          <li>If you get any wrong Sunday, must pick 6 winners on Monday. If all 6 on Monday are correct, you will advance and will only need to pick one winner on Saturday of the 2nd week.</li>
       </ul>
-        <p>Every following day is also 1 pick.</p>
-        
-        </div>
-        
-        <p>b.If you pick incorrectly, you are out unless you buy back in (see rule 5).</p>
+       <li>This is a survivor pool, and the reason for the above is to make it more difficult for those who have already picked a loser. You can re-enter as many times as you would like, but the last day to re-enter would be Monday of the first weekend.</li> 
+       <li>If you pick any loser on Monday or beyond, you can not re-enter Please note, just because you re-enter, it does not reset the teams you can choose from.</li>
+      
 
-    <h4>Re-entering the Game:</h4>
-
-        Each player has the chance to buy back-in, but it will cost you another entry fee ($20) for each buy-in as well as more games to pick. Below is the schedule for how many games you will need to pick in order to buy back in and advance:
-
-        a) If you get any wrong on Friday, must pick 4 winners on Saturday. If all 4 on Saturday are correct, you will advance and will only need to pick 1 winner Sunday.
-
-        b) If you get any wrong on Saturday, must pick 5 winners on Sunday. If all 5 on Sunday are correct, you will advance and will only need to pick 1 winner Monday.
-
-        c) If you get any wrong Sunday, must pick 6 winners on Monday. If all 6 on Monday are correct, you will advance and will only need to pick one winner on Saturday of the 2nd week.
-        </p>
-        <p>This is a survivor pool, and the reason for the above is to make it more difficult for those who have already picked a loser. You can buy-in as many times as you would like, but the last day to buy in would be Monday of the first weekend.  If you pick any loser on Monday or beyond, you can not buy back in.  And remember, just because you buy back in, it does not reset the teams you can choose from.  Each team you have chosen before is not available for selection again.  
-
-        In the event that you have advanced and do not have anyone else remaining to choose from because you have chosen all of the teams before that round, you would still tie any player that does not correctly guess the next round.  For instance, if I were to have no remaining teams left after Sunday, but one other person was left and they did not pick a winner on Saturday, we would tie and split the pot.</p>
-      </p>
-
+       <li>In the event that you have advanced and do not have anyone else remaining to choose from, you would still tie any player that does not correctly guess the next round.</li>
+       <li>For instance, if Steve were to have no remaining teams left after Sunday, but one other person was left and they did not pick a winner on Saturday, Steve and Dave would tie and split the pot.</li>
+      </ol>
   </section>
         </div>
 <?php
